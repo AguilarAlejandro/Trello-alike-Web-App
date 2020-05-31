1964c5467c364e4b16258cd651008bed4cf51c83
AguilarAlejandro <57149085+AguilarAlejandro@users.noreply.github.com>
2020-05-31 01:19:47 -0400

    Update database.js

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 //const { NODEMONGO_MONGODB_HOST, NODEMONGO_MONGODB_DATABASE, DBPASSWORD} = process.env;
 //const MONGODB_URI = `mongodb://${NODEMONGO_MONGODB_HOST}/${NODEMONGO_MONGODB_DATABASE}`;
-const MONGODB_URI = `mongodb+srv://Alejandro:<DBPASSWORD>@padmidb-d8iu9.mongodb.net/test?retryWrites=true&w=majority`;
+const MONGODB_URI = `mongodb+srv://Alejandro:${DBPASSWORD}@padmidb-d8iu9.mongodb.net/test?retryWrites=true&w=majority`;
 
 mongoose.connect(MONGODB_URI, {
     useUnifiedTopology: true,
